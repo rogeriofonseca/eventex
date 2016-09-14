c8d3ed56940fb880c5e59b7d35742b5ad21e78f9
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2016-09-14 01:04:58 -0300

    Atualiza urls.py

--- eventex/urls.py
+++ eventex/urls.py
@@ -17,9 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 # importe explicitamente o modulo da view e passe a funcao view como 
 # parametro para a funcao url()
-from eventex.core import views as eventex_views
+from eventex.core.views import home
 
 urlpatterns = [
-    url(r'^$', eventex_views.home),
+    url(r'^$', home),
     url(r'^admin/', admin.site.urls),
 ]
