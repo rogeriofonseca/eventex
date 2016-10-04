8004a3d960ad32d292ccb1ce2e9f45324100e312
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2016-10-04 00:47:21 -0300

    Template email

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -32,15 +32,15 @@ class SubscribeTest(TestCase):
         self.assertIsInstance(form, SubscriptionForm)
 
     def test_form_has_fields(self):
-      """Form must have 4 fields.""" 
-      form = self.resp.context['form']
-      self.assertSequenceEqual(['name','cpf','email','phone'], list(form.fields))
+        """Form must have 4 fields."""
+        form = self.resp.context['form']
+        self.assertSequenceEqual(['name','cpf','email','phone'], list(form.fields))
 
 
 class SubscribePostTest(TestCase):
     def setUp(self):
         data = dict(name='Henrique Bastos', cpf='12345678901',
-            email='henrique@bastos.net', phone='21-99321-1234')
+                    email='henrique@bastos.net', phone='21-99321-1234')
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
@@ -49,3 +49,31 @@ class SubscribePostTest(TestCase):
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
+
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = 'Confirmação de inscrição'
+
+        self.assertEqual(expect, email.subject)
+
+    def test_subscription_email_from(self):
+        email = mail.outbox[0]
+        expect = 'contato@eventex.com.br'
+
+        self.assertEqual(expect, email.from_email)
+
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expect = ['contato@eventex.com.br', 'henrique@bastos.net']
+
+        self.assertEqual(expect, email.to)
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+
+        self.assertIn('Henrique Bastos', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('henrique@bastos.net', email.body)
+        self.assertIn('21-99618-6180', email.body)
+
