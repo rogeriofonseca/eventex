d68ac131e5b5d8a05ca61f11311c178ba5f78eeb
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2016-10-04 22:48:21 -0300

    Criando template de email; Configurando envio de email;

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -40,7 +40,7 @@ class SubscribeTest(TestCase):
 class SubscribePostTest(TestCase):
     def setUp(self):
         data = dict(name='Henrique Bastos', cpf='12345678901',
-                    email='henrique@bastos.net', phone='21-99321-1234')
+                    email='henrique@bastos.net', phone='21-99618-6180')
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
@@ -77,3 +77,21 @@ class SubscribePostTest(TestCase):
         self.assertIn('henrique@bastos.net', email.body)
         self.assertIn('21-99618-6180', email.body)
 
+class SubscribeInvalidPost(TestCase):
+    def setUp(self):
+        self.resp = self.client.post('/inscricao/',{})
+
+    def test_post(self):
+        """Invalid POST should not redirect"""
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
+
+    def test_has_form(self):
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_errors(self):
+        form = self.resp.context['form']
+        self.assertTrue(form.errors)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,37 +1,25 @@
-from django.core import mail 
-from django.http import HttpResponseRedirect 
+from django.core import mail
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-from eventex.subscriptions.forms import SubscriptionForm 
+from django.template.loader import render_to_string
+from eventex.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
     if request.method == 'POST':
-        mail.send_mail('Confirmação de inscrição',
-                       MESSAGE,
-                       'contato@eventex.com.br',
-                       ['contato@eventex.com.br','henrique@bastos.net'])
-        return HttpResponseRedirect('/inscricao/')
+        form = SubscriptionForm(request.POST)
+
+        if form.is_valid():
+            body = render_to_string('subscriptions/subscription_email.txt',
+                                form.cleaned_data)
+
+            mail.send_mail('Confirmação de inscrição',
+                           body,
+                           'contato@eventex.com.br',
+                           ['contato@eventex.com.br', form.cleaned_data['email']])
+            return HttpResponseRedirect('/inscricao/')
+        else:
+            return render(request, 'subscriptions/subscription_form.html',
+                          {'form': form})
     else:
         context = {'form': SubscriptionForm()}
         return render(request, 'subscriptions/subscription_form.html', context)
-
-MESSAGE = """
-Olá! Tudo bem?
-
-Muito obrigada por se inscrever no Eventex.
-
-Estes foram os dados que você nos forneceu
-em sua inscrição:
-
-Nome: Henrique Bastos
-CPF: 12345678901
-Email: henrique@bastos.net
-Telefone: 21-99618-6180
-
-Em até 48 horas úteis, a nossa equipe entrará
-em contato com você para concluirmos a sua
-matrícula.
-
-Atenciosamente,
---
-Morena
-"""
