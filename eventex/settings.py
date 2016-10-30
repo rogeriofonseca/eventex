5fd70982c13d3a7bf7fa460f47b6badf43bfadba
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2016-10-30 01:30:09 -0200

    correcao em settings.

--- eventex/settings.py
+++ eventex/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
-import os
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from decouple import config, Csv
