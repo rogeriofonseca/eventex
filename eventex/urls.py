dc74e68dbe14abeb3096b0cbe5e1f974bd50e644
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2016-11-20 21:21:14 -0200

    Thank you page

--- eventex/urls.py
+++ eventex/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 # importe explicitamente o modulo da view e passe a funcao view como 
 # parametro para a funcao url()
 from eventex.core.views import home
-from eventex.subscriptions.views import subscribe
+from eventex.subscriptions.views import subscribe, detail
 
 urlpatterns = [
     url(r'^$', home),
     url(r'^inscricao/$', subscribe),
+    url(r'^inscricao/(\d+)/$', detail),
     url(r'^admin/', admin.site.urls),
 ]
